bba44f6317b8b69c75320df037ee4c4feb4aad16
c1145@oic.jp <c1145@oic.jp>
2022-11-24 11:28:23 +0900

    メッセージ接続クラス追加

---
+++ SalesManagement_SysDev/common/MessageDsp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesManagement_SysDev
+{
+    class MessageDsp
+    {
+        public DialogResult MsgDsp(string MsgNumber)
+        {
+            DialogResult result=DialogResult.None;
+
+            try
+            {
+                var context = new SalesManagement_DevContext();
+                var message = context.M_Messages.Single(x => x.MsgCD == MsgNumber);
+                MessageBoxButtons btn = (MessageBoxButtons)message.MsgButton;
+                MessageBoxIcon icon = (MessageBoxIcon)message.MsgICon;
+                result = MessageBox.Show(message.MsgComments, message.MsgTitle, btn, icon);
+
+                context.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result;
+        }
+    }
+}
