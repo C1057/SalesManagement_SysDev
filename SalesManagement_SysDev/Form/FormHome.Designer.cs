cdf19fe93ad3c2fc8bc264c937a9eec56e86651c
c1145@oic.jp <c1145@oic.jp>
2022-11-24 13:27:22 +0900

    tyoihennkou

--- SalesManagement_SysDev/Form/FormHome.Designer.cs
+++ SalesManagement_SysDev/Form/FormHome.Designer.cs
@@ -433,7 +433,6 @@ namespace SalesManagement_SysDev
             this.buttonControl = new SalesManagement_SysDev.maruibutton();
             this.buttonLogin = new SalesManagement_SysDev.maruibutton();
             this.buttonLogout = new SalesManagement_SysDev.maruibutton();
-            this.button1 = new System.Windows.Forms.Button();
             this.panelHeader.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panelHattyu.SuspendLayout();
@@ -5096,23 +5095,12 @@ namespace SalesManagement_SysDev
             this.buttonLogout.Text = "ログアウト";
             this.buttonLogout.UseVisualStyleBackColor = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(944, 173);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(262, 57);
-            this.button1.TabIndex = 217;
-            this.button1.Text = "データベース生成";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // FormHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
             this.ClientSize = new System.Drawing.Size(1924, 1061);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.maruibutton10);
             this.Controls.Add(this.buttonHumberger);
             this.Controls.Add(this.labelWarning);
@@ -5603,7 +5591,6 @@ namespace SalesManagement_SysDev
         private System.Windows.Forms.TextBox textBoxHaProductName;
         private System.Windows.Forms.Label labelHaEmployeeName;
         private System.Windows.Forms.TextBox textBoxHaEmployeeName;
-        private System.Windows.Forms.Button button1;
     }
 }
 
