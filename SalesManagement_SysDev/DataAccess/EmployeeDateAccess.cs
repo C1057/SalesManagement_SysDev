a31b766bc32e20d440431c71303afb20c1e7a0ca
奥田絢士郎 <C1074@oic.jp>
2022-12-01 16:24:42 +0900

    社員管理登録モジュール作成

--- SalesManagement_SysDev/DataAccess/EmployeeDateAccess.cs
+++ SalesManagement_SysDev/DataAccess/EmployeeDateAccess.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SalesManagement_SysDev
 {
     class EmployeeDateAccess
     {
+        MessageDsp msg = new MessageDsp();
+
+
+        /// <summary>
+        /// 社員登録モジュール
+        /// </summary>
+        /// <param name="addData"></param>
+        /// <return>void</return>
+        
+        
+
+        public void addEmployee(M_Employee addData)
+        {
+            DialogResult result = msg.MsgDsp("M5021"); //登録確認メッセージ
+            if (result == DialogResult.Cancel)　//登録Cancel　登録モジュールを終了する
+            {
+                return;
+            }
+
+            try
+            {
+                var context = new SalesManagement_DevContext();
+
+                //パスワードをハッシュ化
+                　
+                context.M_Employees.Add(addData); //データをセット
+                context.SaveChanges();　　//
+                context.Dispose();  //　contextを開放
+
+                msg.MsgDsp("M5022");　//登録完了メッセージ
+            }
+            catch
+            {
+                msg.MsgDsp("M5023"); //登録失敗メッセージ
+            }
+
+        }
     }
 }
