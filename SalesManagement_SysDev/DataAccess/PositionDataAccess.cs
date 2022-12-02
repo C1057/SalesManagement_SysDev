05ed1ee0ce031b3a7b374fc3a7f7f5a06b2f1e59
C1165 <C1165@oic.osaka.jp>
2022-12-02 17:53:26 +0900

    役職管理途中

--- SalesManagement_SysDev/DataAccess/PositionDataAccess.cs
+++ SalesManagement_SysDev/DataAccess/PositionDataAccess.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SalesManagement_SysDev
 {
     class PositionDataAccess
     {
+        MessageDsp msg = new MessageDsp();
+        public void DeletePosition(int PositionID)
+        {
+            DialogResult result = msg.MsgDsp("M14001");
+            if (result == DialogResult.Cancel)
+            {
+                return;
+            }
+            var context = new SalesManagement_DevContext();
+            var position = context.M_Positions.Single(x => x.PoID == PositionID);
+            position.PoFlag = 2;
+            context.SaveChanges();
+            context.Dispose();
+
+            msg.MsgDsp("M14002");
+        }
     }
 }
