670ff8e82cf1d06cd325e3ae2746bc2d4f8517ab
奥田絢士郎 <C1074@oic.jp>
2022-12-09 13:57:59 +0900

    出庫完成

--- SalesManagement_SysDev/DataAccess/SyukkoDateAccess.cs
+++ SalesManagement_SysDev/DataAccess/SyukkoDateAccess.cs
@@ -41,7 +41,7 @@ namespace SalesManagement_SysDev
 
 
             var Syukko = context.T_Syukkos.Single(x => x.SyID == ConfirmData.SyID); //出庫テーブルのデータを変更する
-            Syukko.EmID = 
+            Syukko.EmID = ConfirmData.EmID;
             Syukko.SyFlag = 1;
             Syukko.SyHidden = DateTime.Now.ToString();
 
@@ -54,32 +54,76 @@ namespace SalesManagement_SysDev
 
 
         /// <summary>
-        /// 入庫非表示機能
+        /// 出庫非表示機能
         /// </summary>
-        /// <param name="WarehousingID"></param>
-        /// <returns>List<t_Warehosing></returns>
-        public void DeleteWarehousing(int WarehousingID) //非表示
+        /// <param name="SyukkoID"></param>
+        /// <returns>List<t_Syukko></returns>
+        public void DeleteSyukko(int SyukkoID) //非表示
         {
             var context = new SalesManagement_DevContext();                             //SalesManagement_DevContextクラスのインスタンス化
-            var Warehousing = context.T_Warehousings.Single(x => x.WaID == WarehousingID);             //非表示にするレコードの抽出
-            Warehousing.WaFlag = 2;                                                          //入庫管理フラグを2にする
+            var Syukko = context.T_Syukkos.Single(x => x.SyID == SyukkoID);             //非表示にするレコードの抽出
+            Syukko.SyFlag = 2;                                                          //入庫管理フラグを2にする
             context.SaveChanges();                                                      //更新を確定する
             context.Dispose();                                                          //contextを解放
         }
 
         /// <summary>
-        /// 入庫情報取得モジュール
+        /// 出庫情報取得モジュール
         /// </summary>
         /// <param>なし</param>
-        /// <returns>List<T_Warehousing></returns>
-        public List<T_Warehousing> GetData()
+        /// <returns>List<T_Syukko></returns>
+        public List<T_Syukko> GetData()
         {
             var context = new SalesManagement_DevContext();             //SalesManagement_DevContextクラスのインスタンス化
-            return context.T_Warehousings.ToList();                          //入庫テーブルの全データを戻り値として返す
+            return context.T_Syukkos.ToList();                          //入庫テーブルの全データを戻り値として返す
         }
 
 
 
+        /// <summary>
+        /// 出庫検索モジュール　（ID系）
+        /// </summary>
+        /// <param name="methodflg"></param>
+        /// <param name="SearchInfo"></param>
+        /// <returns>List<T_Syukko></returns>
+        public List<T_Syukko> SearchEmployee(int methodflg, string SearchInfo)　//IDで検索
+        {
+            var context = new SalesManagement_DevContext();
+            List<T_Syukko> SearchResult = null;
+
+            if (methodflg == 1)　//社員IDで検索
+            {
+                int SyukkoID = int.Parse(SearchInfo);
+                SearchResult = context.T_Syukkos.Where(x => x.SyID == SyukkoID).ToList();
+            }
+
+            else if (methodflg == 2)　//社員IDで検索
+            {
+                int EmployeeID = int.Parse(SearchInfo);
+                SearchResult = context.T_Syukkos.Where(x => x.EmID == EmployeeID).ToList();
+            }
+
+            else if (methodflg == 3)　//顧客IDで検索
+            {
+                int ClientID = int.Parse(SearchInfo);
+                SearchResult = context.T_Syukkos.Where(x => x.ClID == ClientID).ToList();
+            }
+
+            else if (methodflg == 4)　//営業所IDで検索
+            {
+                int SalesOfficeID = int.Parse(SearchInfo);
+                SearchResult = context.T_Syukkos.Where(x => x.SoID == SalesOfficeID).ToList();
+            }
+            else if (methodflg == 5)　//受注IDで検索
+            {
+                int OrderID = int.Parse(SearchInfo);
+                SearchResult = context.T_Syukkos.Where(x => x.OrID == OrderID).ToList();
+            }
+
+
+
+            return SearchResult;
+        }
 
 
 
