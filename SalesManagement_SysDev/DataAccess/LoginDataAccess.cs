4313f4da6f93180baced91e273d6c535db925ca7
c1145@oic.jp <c1145@oic.jp>
2023-01-13 16:51:32 +0900

    ログイン、ログアウト機能完成

--- SalesManagement_SysDev/DataAccess/LoginDataAccess.cs
+++ SalesManagement_SysDev/DataAccess/LoginDataAccess.cs
@@ -9,7 +9,8 @@ namespace SalesManagement_SysDev
 {
     class LoginDataAccess
     {
-        PasswordHash hash = new PasswordHash(); // 
+        PasswordHash hash = new PasswordHash();                 //ハッシュクラスのインスタンス化 
+        DataInputCheck InputCheck = new DataInputCheck();       //データチェッククラスのインスタンス化
 
         /// <summary>
         /// ログインモジュール
@@ -17,30 +18,31 @@ namespace SalesManagement_SysDev
         /// <param name="EmployeeID"></param>
         /// <param name="Password"></param>
         /// <returns><M_Employee></returns>
-        public bool Login(int EmployeeID, string Password) //ログイン
+        public bool Login(string EmployeeID, string Password) //ログイン
         {
             var context = new SalesManagement_DevContext();  //SalesManagement_DevContextのcontext化
+            
+            if (!InputCheck.EmployeeIDInputCheck(EmployeeID))
+            {
+                return false;
+            }
 
-            M_Employee EmployeeData = context.M_Employees.Single(Employee => Employee.EmID == EmployeeID); //
-            string HashPassword = hash.GeneratePasswordHash(Password, EmployeeData.EmSalt);  //
+            int EmID = int.Parse(EmployeeID);           //社員ID用変数に代入
+            M_Employee EmployeeData = context.M_Employees.Single(Employee => Employee.EmID == EmID); //IDと一致する社員データを取得する
+            string HashPassword = hash.GeneratePasswordHash(Password, EmployeeData.EmSalt);  //入力されたパスワードと社員データのソルトを使ってハッシュ化する
 
-            if (HashPassword == EmployeeData.EmPassword)
+            if (HashPassword == EmployeeData.EmPassword)            //ハッシュ化した入力パスワードと社員データのパスワードが一致したらtrueを返す
             {
                 context.Dispose();　//contextを開放
                 return true; //trueを返す
-
-
             }
-
             else
-            {　
+            {
+                MessageBox.Show("パスワードが間違っています", "ログイン確認", MessageBoxButtons.OK, MessageBoxIcon.Error);     //パスワードが間違っている場合のエラーメッセージ
+
                 context.Dispose();　//contextを開放
                 return false; //falseを返す
             }
-
-
         }
-            
-            
     }
 }
